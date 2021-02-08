b65b38aa05a296001e921afcb806942ceb8641b7
Bakhtiyar Zubairov <murderer187@mail.ru>
2021-02-08 14:15:46 +0100

    Update text box tests. Added steps

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class TextBoxTests {
     @BeforeAll
@@ -18,18 +20,26 @@ public class TextBoxTests {
     @Test
     void fillFormTest() {
 
-        open("https://demoqa.com/text-box");
+        step("Open main page", () -> {
+                    final String url = "https://demoqa.com/text-box";
+                    open(url);
+                    Allure.link("Testing", url);
+                });
 
-        $("#userName").val("Rodrigo");
-        $("#userEmail").val("daSilva@company.com");
-        $("#currentAddress").val("Oslo");
-        $("#permanentAddress").val("Street 1");
-        $("#submit").click();
+        step("Fill the forms", () -> {
+                    $("#userName").val("Rodrigo");
+                    $("#userEmail").val("daSilva@company.com");
+                    $("#currentAddress").val("Oslo");
+                    $("#permanentAddress").val("Street 1");
+                    $("#submit").click();
+                });
 
-        $("#output").shouldHave(text("Name:Rodrigo\n" +
-                "Email:daSilva@company.com\n" +
-                "Current Address :Oslo\n" +
-                "Permananet Address :Street 1"));
+        step("Verify text as expected", () -> {
+                    $("#output").shouldHave(text("Name:Rodrigo\n" +
+                            "Email:daSilva@company.com\n" +
+                            "Current Address :Oslo\n" +
+                            "Permanent Address :Street 1"));
+                });
     }
 
     @Test
@@ -40,41 +50,61 @@ public class TextBoxTests {
                 permanentAddress = "Street 1";
 
 
-        open("https://demoqa.com/text-box");
+        step("Open main page", () -> {
+            final String url = "https://demoqa.com/text-box";
+            open(url);
+            Allure.link("Testing", url);
+        });
 
-        $("#userName").val(userName);
-        $("#userEmail").val(email);
-        $("#currentAddress").val(currentAddress);
-        $("#permanentAddress").val(permanentAddress);
-        $("#submit").click();
+        step("Fill the forms", () -> {
+            $("#userName").val(userName);
+            $("#userEmail").val(email);
+            $("#currentAddress").val(currentAddress);
+            $("#permanentAddress").val(permanentAddress);
+            $("#submit").click();
+        });
 
-        $("#output").shouldHave(text(
-                "Name:" + userName + "\n" +
-                        "Email:Doe@firm.com\n" +
-                        "Current Address :Oslo\n" +
-                        "Permanent Address :Street 1"));
+        step("Verify text as expected", () -> {
+            $("#output").shouldHave(text(
+                    "Name:" + userName + "\n" +
+                            "Email:Doe@firm.com\n" +
+                            "Current Address :Oslo\n" +
+                            "Permanent Address :Street 1"));
+        });
 
-        $("#output").shouldHave(text(String.format(
-                "Name:%s\n" +
-                        "Email:%s\n" +
-                        "Current Address :Oslo\n" +
-                        "Permanent Address :Street 1", userName, email)));
+        step("Verify format as expected", () -> {
+            $("#output").shouldHave(text(String.format(
+                    "Name:%s\n" +
+                            "Email:%s\n" +
+                            "Current Address :Oslo\n" +
+                            "Permanent Address :Street 1", userName, email)));
+        });
 
-        $("#name").shouldHave(text(userName));
+        step("Verify user name", () -> {
+            $("#name").shouldHave(text(userName));
+        });
     }
 
 
     @Test
     void wrongEmailTest() {
-        open("https://demoqa.com/text-box");
+        step("Open main page", () -> {
+            final String url = "https://demoqa.com/text-box";
+            open(url);
+            Allure.link("Testing", url);
+        });
 
-        $("#userName").val("John");
-        $("#userEmail").val("Doe");
-        $("#currentAddress").val("Montenegro");
-        $("#permanentAddress").val("Street 1");
-        $("#submit").click();
+        step("Fill the forms with unexpected email", () -> {
+            $("#userName").val("John");
+            $("#userEmail").val("Doe");
+            $("#currentAddress").val("Montenegro");
+            $("#permanentAddress").val("Street 1");
+            $("#submit").click();
+        });
 
-        $("#userEmail").shouldHave(cssClass("field-error"));
+        step("Verify user email has error alert", () -> {
+            $("#userEmail").shouldHave(cssClass("field-error"));
+        });
 
     }
 }
